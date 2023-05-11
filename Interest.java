12d3d0978c7f4c56df479df97c70c0182b72f022
Akshat sahu <akshatsahu2204@gmail.com>
2023-05-11 13:41:03 +0530

    Add Interest.java

---
+++ Interest.java
@@ -0,0 +1,10 @@
+public class Interest {
+    public static void main(String[] args) {
+        double originalValue = 80;
+        double updatedValue = 90;
+
+        double percentageDifference = ((updatedValue - originalValue) / originalValue) * 100;
+        System.out.println("The percentage difference is " + percentageDifference + "%.");
+    }
+    
+}
