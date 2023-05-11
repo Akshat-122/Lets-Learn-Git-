a42bd335c2ddfe620af5f241b3a8ce7541905525
Akshat sahu <akshatsahu2204@gmail.com>
2023-05-11 13:45:07 +0530

    Add per2.java

---
+++ per2.java
@@ -0,0 +1,13 @@
+public class per2 {
+
+
+    
+	public static void main(String[] args) {
+		double originalValue = 7.5;
+		double updatedValue = 7.2;
+
+		double percentageDifference = ((updatedValue - originalValue) / originalValue) * 100;
+		System.out.println("The percentage difference is " + percentageDifference + "%.");
+	}
+    
+}
