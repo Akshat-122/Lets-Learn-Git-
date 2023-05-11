e8efaf99fc9a73f203da83234bd2e2ae7516c876
Akshat sahu <akshatsahu2204@gmail.com>
2023-05-11 13:31:23 +0530

    Add ex.java

---
+++ ex.java
@@ -0,0 +1,16 @@
+public class ex {
+
+   
+	
+       
+            public static void main(String[] args) {
+                double x = 5;
+                double result = (x + 3)/6 - ((2 * x + 5)/3);
+                System.out.println(result);
+            }
+        }
+    
+    
+    
+    
+
